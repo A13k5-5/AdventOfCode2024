9f9c76cc0587325f1060bf1f9705575894c7c521
Alex Pison <pisonalex@gmail.com>
2024-12-12 01:04:59 +0100

    Day4 done

--- day4/day4.py
+++ day4/day4.py
@@ -52,11 +52,32 @@ def check_diagonal_lr_up(rows, target):
     return acc
 
 
-rows = load_input("input.txt")
-the_word = "XMAS"
-acc = 0
-acc += check_horizontal(rows, the_word)
-acc += check_vertical(rows, the_word)
-acc += check_diagonal_lr_down(rows, the_word)
-acc += check_diagonal_lr_up(rows, the_word)
-print(acc)
+def star1():
+    rows = load_input("input.txt")
+    the_word = "XMAS"
+    acc = 0
+    acc += check_horizontal(rows, the_word)
+    acc += check_vertical(rows, the_word)
+    acc += check_diagonal_lr_down(rows, the_word)
+    acc += check_diagonal_lr_up(rows, the_word)
+    print(acc)
+
+
+def star2():
+    rows = load_input("input.txt")
+    the_word = "MAS"
+    acc = 0
+    for y in range(len(rows) - (len(the_word) - 1)):
+        for x in range(len(rows[0]) - (len(the_word) - 1)):
+            matrix = []
+            matrix.append(rows[y][x : (x + len(the_word))])
+            matrix.append(rows[y + 1][x : (x + len(the_word))])
+            matrix.append(rows[y + 2][x : (x + len(the_word))])
+            acc += (
+                check_diagonal_lr_down(matrix, the_word)
+                + check_diagonal_lr_up(matrix, the_word)
+            ) // 2
+    print(acc)
+
+
+star2()
