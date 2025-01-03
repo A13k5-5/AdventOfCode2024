e290b014769cc3258b8ab70d2be1b339d7d98070
Alex Pison <pisonalex@gmail.com>
2025-01-03 23:12:28 +0100

    Day6 finished

--- day6/day6.py
+++ day6/day6.py
@@ -1,3 +1,6 @@
+import copy
+
+
 def load_map(filename):
     map = []
     with open(filename, "r") as input:
@@ -32,15 +35,19 @@ def can_move_forward(map, x, y, horizontal, vertical):
     )
 
 
-def move_forward(map, x, y, cur_dir):
+def move_forward(visited, map, x, y, cur_dir):
     # 0 - North, 1 - East, 2 - South, 3 - West
     vertical = [-1, 0, 1, 0][cur_dir]
     horizontal = [0, 1, 0, -1][cur_dir]
-    map[y][x] = "X"
+    # map[y][x] = "X"
+    in_loop = str(cur_dir) in map[y][x]
+    map[y][x] += str(cur_dir)
+    visited.add((x, y))
     if can_move_forward(map, x, y, horizontal, vertical):
-        return (x + horizontal, y + vertical), cur_dir
+        return (x + horizontal, y + vertical), cur_dir, in_loop
     else:
-        return (x, y), (cur_dir + 1) % 4
+        # turn right
+        return (x, y), (cur_dir + 1) % 4, in_loop
 
 
 def out_of_map(map, x, y):
@@ -48,12 +55,34 @@ def out_of_map(map, x, y):
 
 
 def star1():
-    map = load_map("input.txt")
+    orig_map = load_map("input.txt")
+    map = copy.deepcopy(orig_map)
+    start_pos = find_guard(map)
     cur_pos = find_guard(map)
     cur_dir = get_dir(map, cur_pos[0], cur_pos[1])
+    visited = set()
     while not out_of_map(map, cur_pos[0], cur_pos[1]):
-        cur_pos, cur_dir = move_forward(map, cur_pos[0], cur_pos[1], cur_dir)
-    print(("".join(["".join(line) for line in map])).count("X"))
+        cur_pos, cur_dir, in_loop = move_forward(
+            visited, map, cur_pos[0], cur_pos[1], cur_dir
+        )
+    visited.remove(start_pos)
+    loop = 0
+    for cell in visited:
+        map_copy = copy.deepcopy(orig_map)
+        map_copy[cell[1]][cell[0]] = "#"
+        cur_pos = start_pos
+        cur_dir = get_dir(map_copy, cur_pos[0], cur_pos[1])
+        in_loop = False
+        print(loop)
+        while not out_of_map(map_copy, cur_pos[0], cur_pos[1]):
+            cur_pos, cur_dir, in_loop = move_forward(
+                set(), map_copy, cur_pos[0], cur_pos[1], cur_dir
+            )
+            if in_loop:
+                loop += 1
+                break
+
+    print(loop)
 
 
 star1()
