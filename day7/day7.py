0726fca4851ff2d82b5b50c0df33fe86f0c66df2
Alex Pison <pisonalex@gmail.com>
2025-01-06 22:31:37 +0100

    Star 1 day 7 done

--- day7/day7.py
+++ day7/day7.py
@@ -9,5 +9,31 @@ def load_input(filename):
     return eqs
 
 
+def perform_action(nums, operation, results):
+    if len(nums) == 1:
+        results.add(nums[0])
+        # print(nums[0])
+        return
+
+    new_nums = []
+    new_nums.append(nums[0] * nums[1] if operation == "mult" else nums[0] + nums[1])
+    new_nums += nums[2:]
+    perform_action(new_nums, "mult", results)
+    perform_action(new_nums, "add", results)
+
+
 def star1():
     eqs = load_input("input.txt")
+    counter = 0
+    for eq in eqs:
+        wanted_result = eq[0]
+        nums = eq[1:]
+        results = set()
+        perform_action(nums, "mult", results)
+        perform_action(nums, "add", results)
+        if wanted_result in results:
+            counter += wanted_result
+    print(counter)
+
+
+star1()
